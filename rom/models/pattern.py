c136211113a69802cf3f5c0d9f58a290e0956d20
Caroline Nardi <caroline.m.nardi@gmail.com>
2018-07-21 20:25:04 -0700

    Created relation between stops and patterns

--- rom/models/pattern.py
+++ rom/models/pattern.py
@@ -1,6 +1,6 @@
 from django.contrib.gis.db import models
 from .route import Route
-# from .stop import Stop
+from .stop import Stop
 
 class Pattern(models.Model):
     route = models.ForeignKey(Route, on_delete=models.CASCADE)
@@ -9,7 +9,7 @@ class Pattern(models.Model):
     sa_trips = models.IntegerField(default=0)
     su_trips = models.IntegerField(default=0)
     core_trips = models.IntegerField(default=0)
-    # stops = models.ManyToManyField(Stop)
+    stops = models.ManyToManyField(Stop)
 
     def __str__(self):
         return "%s Pattern %s" %(self.route.operator.name, self.pattern_onestop_id)
