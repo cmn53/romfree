ce9b1d6e01710653315350e635baf68ac397d8c9
Caroline Nardi <caroline.m.nardi@gmail.com>
2018-07-21 19:45:04 -0700

    Created pattern model and migrated data

---
+++ rom/models/pattern.py
@@ -0,0 +1,15 @@
+from django.contrib.gis.db import models
+from .route import Route
+# from .stop import Stop
+
+class Pattern(models.Model):
+    route = models.ForeignKey(Route, on_delete=models.CASCADE)
+    pattern_onestop_id = models.CharField(max_length=100)
+    wk_trips = models.IntegerField(default=0)
+    sa_trips = models.IntegerField(default=0)
+    su_trips = models.IntegerField(default=0)
+    core_trips = models.IntegerField(default=0)
+    # stops = models.ManyToManyField(Stop)
+
+    def __str__(self):
+        return "%s Pattern %s" %(self.route.operator.name, self.pattern_onestop_id)
